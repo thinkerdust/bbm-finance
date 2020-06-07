9d0d21de182cbf03f737e261cce8fdc129fd4e34
thinkerdust <ayurakhma1@gmail.com>
2020-06-08 00:56:49 +0700

    add login page and dashboard theme

---
+++ resources/views/login.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Login Page</title>
+        <link rel="stylesheet" href="{{ asset('/assets/bower-components/bootstrap/dist/css/bootstrap.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('/assets/css/login-style.css') }}">
+    </head>
+    <body>
+        <div class="login-form">    
+            <form action="/examples/actions/confirmation.php" method="post">
+		        <div class="avatar"><i class="material-icons">&#xE7FF;</i></div>
+                    <h4 class="modal-title">Login to Your Account</h4>
+                <div class="form-group">
+                    <input type="text" class="form-control" placeholder="Username" required="required" autocomplete="off">
+                </div>
+                <div class="form-group">
+                    <input type="password" class="form-control" placeholder="Password" required="required" autocomplete="off">
+                </div>
+                <div class="form-group small clearfix">
+                    <label class="checkbox-inline"><input type="checkbox"> Remember me</label>
+                    <a href="#" class="forgot-link">Forgot Password?</a>
+                </div> 
+                <input type="submit" class="btn btn-primary btn-block btn-lg" value="Login">              
+            </form>			
+            <div class="text-center small">Don't have an account? <a href="#">Sign up</a></div>
+        </div>
+        <script src="{{ asset('/assets/bower-components/jquery/jquery.min.js') }}"></script>
+        <script src="{{ asset('/assets/bower-components/bootstrap/dist/js/bootstrap.min.js') }}"></script>
+    </body>
+</html>
